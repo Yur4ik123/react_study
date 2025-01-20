9a64735aea8e53812fc9d88e58ea36e3d0069a90
Yurii Malishko <yur.malishko@gmail.com>
2025-01-20 13:37:51 +0200

    continue

---
+++ app/(site)/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import type {Metadata} from "next";
+import {Button, Htag, Ptag, Rating, Tag} from "@/components";
+import styles from "./page.module.css";
+import {useState} from "react";
+
+
+// export const metadata: Metadata = {
+//   title: "Исправленные данные",
+//   description: "Описание ",
+// };
+
+// export async function generateMetadata(): Promise<Metadata> {
+//     // data
+//     return {
+//         title: 'Computed metadata'
+//     }
+// }
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+
+        </main>
+    );
+}
